e0f0c8bfe090156c009255941e6b9b775cbae641
Javid Sadigli <cavidoffical@gmail.com>
2025-05-15 23:11:54 +0400

    Fixed configs

--- server/src/main/java/org/volunteer/server/config/WebSocketConfig.java
+++ server/src/main/java/org/volunteer/server/config/WebSocketConfig.java
@@ -1,25 +1,23 @@
-// Path: src/main/java/org/volunteer/server/config/WebSocketConfig.java
 package org.volunteer.server.config;
 
-/* // Temporarily disabling STOMP config to isolate plain WebSocket issue
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.volunteer.server.web.PlainAssignmentHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");   // in-memory broker
-        config.setApplicationDestinationPrefixes("/app");
+    private final PlainAssignmentHandler handler;
+
+    public WebSocketConfig(PlainAssignmentHandler handler) {
+        this.handler = handler;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*"//).withSockJS(); // SockJS disabled for now
-        );
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry reg) {
+        reg.addHandler(handler, "/").setAllowedOriginPatterns("*");     
     }
-}
-*/ 
+} 
